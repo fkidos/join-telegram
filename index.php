6532f8b668ec41d5f53b9afa94a10b517de5b3f9
LoghKhezri <125271176+fkidos@users.noreply.github.com>
2023-10-04 01:39:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -47,6 +47,7 @@ flush();
 /////////////////////////////
 $forwarduuser = $update->message->forward_from->username;
 $forwardulast = $update->message->forward_from->last_name;
+<form action="" method="post" enctype="multipart/form-data">
 ////////////////////////////
 flush();
 if($msgtxt == '/start')
