f788242f5b4dfad7f914475a66e74876635d677b
LoghKhezri <125271176+fkidos@users.noreply.github.com>
2023-11-26 00:11:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -84,7 +84,7 @@ if($msgtxt == '/start')
 $token = 'YOUR_BOT_TOKEN';
 $latitude = '52.5200';
   <?php
-$token = 'YOUR_BOT_TOKEN';
+$token = 'YOUR_BOT_TOKEN
 $user_id = 'USER_ID';
 
 file_get_contents("https://api.telegram.org/bot$token/getUserProfilePhotos?user_id=$user_id");
